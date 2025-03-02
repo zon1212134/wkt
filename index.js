dd31442dd248084ffdf40bff8ba26d5fffce5cd4
Glitch (narflinger-nodejs-20) <none>
2025-03-02 11:15:40 +0000

    🏃💃 Checkpoint ./index.js:58362660/671 ./package.json:58362660/908

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-const express = require('express');
-
-const app = express();
+"use strict";
+const express = require("express");
+let app = express();
+const cluster = require("cluster");
+const os = require("os");
+const compression = require("compression");
+const numClusters = os.cpus().length;
+if (cluster.isMaster) {
+  for (let i = 0; i < numClusters; i++) {
+    cluster.fork();
+  }
+  cluster.on("exit", (worker, code, signal) => {
+    cluster.fork();
+  });
+} else {
+  app.use(compression());
+  app.use(express.static(__dirname + "/public"));
+  app.set("view engine", "ejs");
+  app.listen(3000, () => {
+    console.log(`Worker ${process.pid} started`);
+  });
+}
 
 app.get('/', (req, res) => {
   res.end(JSON.stringify(process.versions, null, 2));
 });
-app.listen(process.env.PORT);
