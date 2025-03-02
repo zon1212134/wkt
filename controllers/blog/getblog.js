2a70dfe88b8b7fd5875ed977ce721b93a3c40382
Glitch (watawata) <none>
2025-03-02 23:53:50 +0000

    💗👁 Checkpoint ./controllers/blog/getblog.js:58362660/2064 ./routes/blog.js:58362660/134

--- controllers/blog/getblog.js
+++ controllers/blog/getblog.js
@@ -6,25 +6,52 @@ const NodeCache = require('node-cache');
 
 const cache = new NodeCache({ stdTTL: 60, checkperiod: 120 });
 
-async function Getblog(blogId) {
+async function Getblog(blogId, part) {
   const cachedData = cache.get(blogId);
   if (cachedData) {
-    console.log('キャッシュからデータを取得');
     return cachedData;
   }
-
   try {
-    const response = await axios.get(`https://wakameblog.glitch.me/blogs/latest`);
-
+    const response = await axios.get(`https://wakameblog.glitch.me/${part}/${blogId}.html`);
     cache.set(blogId, response.data);
-    
-    console.log('APIからデータを取得');
     return response.data;
   } catch (error) {
-    console.error('APIの取得に失敗しました:', error);
     return null;
   }
 }
 
+async function GetblogList(part) {
+  const cachedData = cache.get(`list-${part}`);
+  if (cachedData) {
+    return cachedData;
+  }
+  try {
+    const response = await axios.get(`https://wakameblog.glitch.me/${part}.html`);
+    cache.set(`list-${part}`, response.data);
+    return response.data;
+  } catch (error) {
+    return null;
+  }
+}
+
+router.get('/blog/:id', async (req, res) => {
+  const blogId = req.params.id;
+  const blogData = await Getblog(blogId, "blog");
+  if (blogData) {
+    res.send(blogData);
+  } else {
+    res.status(500).send('ブログ記事の取得に失敗しました');
+  }
+});
+
+router.get('/l/:id', async (req, res) => {
+  const part = req.params.id;
+  const blogData = await GetblogList(part);
+  if (blogData) {
+    res.send(blogData);
+  } else {
+    res.status(500).send('ブログ記事の取得に失敗しました');
+  }
+});
 
 module.exports = router;
