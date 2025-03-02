6c38bae14bdd1fe70b0fefa994a429a0871d07ff
Glitch (watawata) <none>
2025-03-02 14:11:03 +0000

    🏊🏼🗃 Checkpoint ./views/tools/home.ejs:58362660/8270 ./views/tools/tool/android.ejs:58362660/458 ./routes/tools.js:58362660/326

--- routes/tools.js
+++ routes/tools.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
 
 router.get("/", (req, res) => {
   res.render("tools/home");
+});
+
+router.get("/tool/:id", (req, res) => {
+  res.render(`tools/tool/${req.params.id}`);
 });
 
 module.exports = router;
