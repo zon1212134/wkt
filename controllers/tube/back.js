2e91f4b38543fb5b4201d694e8be4afb8d37bed3
Glitch (watawata) <none>
2025-03-03 10:41:58 +0000

    🗼😅 Checkpoint ./views/tube/search.ejs:58362660/1529 ./controllers/tube/back.js:58362660/500 ./package.json:58362660/801 ./views/tube/home.ejs:58362660/4 ./routes/wakametube.js:58362660/1052

--- controllers/tube/back.js
+++ controllers/tube/back.js
@@ -3,6 +3,9 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 const http = require('http');
+const miniget = require('miniget');
+
+const user_agent = process.env.USER_AGENT || "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36";
 
 router.get('/suggest', (req, res) => {
     const keyword = req.query.keyword;
@@ -38,6 +41,19 @@ router.get('/suggest', (req, res) => {
         res.status(500).send({ error: 'えらー。あらら' });
     });
     request.end();
+});
+
+router.get("/vi*", (req, res) => {
+	let stream = miniget(`https://i.ytimg.com/${req.url.split("?")[0]}`, {
+		headers: {
+			"user-agent": user_agent
+		}
+	});
+	stream.on('error', err => {
+		console.log(err);
+		res.status(500).send(err.toString());
+	});
+	stream.pipe(res);
 });
 
 module.exports = router;
--- routes/wakametube.js
+++ routes/wakametube.js
@@ -1,9 +1,52 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const ytsr = require("ytsr");
+
+const limit = process.env.LIMIT || 50;
 
 router.get("/", (req, res) => {
   res.render("tube/home");
+});
+
+router.get("/s", async (req, res) => {
+	let query = req.query.q;
+	let page = Number(req.query.p || 2);
+    try {
+		res.render("tube/search.ejs", {
+			res: await ytsr(query, { limit, pages: page }),
+			query: query,
+			page
+		});
+	} catch (error) {
+		console.error(error);
+		try {
+			res.status(500).render("error.ejs", {
+				title: "ytsr Error",
+				content: error
+			});
+		} catch (error) {
+			console.error(error);
+		}
+	}
+});
+
+router.get("/s", async (req, res) => {
+	let query = req.query.q;
+	let page = Number(req.query.p || 2);
+    try {
+		res.json();
+	} catch (error) {
+		console.error(error);
+		try {
+			res.status(500).render("error.ejs", {
+				title: "ytsr Error",
+				content: error
+			});
+		} catch (error) {
+			console.error(error);
+		}
+	}
 });
 
 router.use("/back", require("../controllers/tube/back"));
