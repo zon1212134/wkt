19043d9414e31ab42144007d0ff994461183523b
Glitch (watawata) <none>
2025-03-03 04:19:33 +0000

    🚔🐂 Checkpoint ./routes/wakametube.js:58362660/9

--- routes/wakametube.js
+++ routes/wakametube.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require("path");
 
 router.get("/", (req, res) => {
-  res.render("tools/home");
+  res.render("tube/home");
 });
 
 router.use("/n", require("../controllers/blog/getblog"));
