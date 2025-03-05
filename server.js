13d4fe43344faf3e3122b0b193907584d3f24ddd
Glitch (watawata) <none>
2025-03-05 10:00:15 +0000

    🚪🍗 Checkpoint ./server/youtube.js:58362660/426 ./server.js:58362660/3179

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
+"use strict";
+
+if (!process.env.NO_CLUSTERS && cluster.isPrimary) {
+  const numClusters = process.env.CLUSTERS || (os.availableParallelism ? os.availableParallelism() : (os.cpus().length || 2))
+
+  console.log(`Primary ${process.pid} is running. Will fork ${numClusters} clusters.`);
+
+  // Fork workers.
+  for (let i = 0; i < numClusters; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died. Forking another one....`);
+    cluster.fork();
+  });
+  
+  return true;
+}
+
 import express from "express";
 import cluster from "cluster";
 import os from "os";
@@ -11,21 +31,15 @@ import wktRouter from "./routes/wakametube.js";
 import gameRouter from "./routes/game.js";
 import sandboxRouter from "./routes/sandbox.js";
 
-import { getVideoInfo } from "./server/youtube.js";
+import YouTubeJS from "youtubei.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const numClusters = os.cpus().length;
 
-if (cluster.isPrimary) {
-  for (let i = 0; i < numClusters; i++) {
-    cluster.fork();
-  }
-  cluster.on("exit", (worker, code, signal) => {
-    cluster.fork();
-  });
-} else {
+
+
   const app = express();
 
   app.use(compression());
@@ -34,22 +48,12 @@ if (cluster.isPrimary) {
   app.set("views", path.join(__dirname, "views"));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.set("trust proxy", 1);
+  
+  let client;
 
   app.get("/", (req, res) => {
     res.render("home/index");
   });
-  
-
-app.get("/info", async (req, res) => {
-  try {
-    const info = await getVideoInfo("dQw4w9WgXcQ");
-    res.json(info);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});  
-  
-  
 
   app.get("/app", (req, res) => {
     res.render("app/list");
@@ -74,8 +78,23 @@ app.get("/info", async (req, res) => {
     res.redirect(`/wkt/s?q=${req.params.des}`);
   });
   app.use("/sandbox", sandboxRouter);
+  
+async function initInnerTube() {
+  try {
+    client = await YouTubeJS.Innertube.create({ location: process.env.GEOLOCATION || "US", cache: new YouTubeJS.UniversalCache(true, process.env.CACHE_DIR || "./.cache") });
 
-  app.listen(3000, () => {
-    console.log(`Worker ${process.pid} started`);
-  });
-}
+    const listener = app.listen(process.env.PORT || 3000, () => {
+      console.log(process.pid, "-- Ready. ytmous is now listening on port", listener.address().port);
+    });
+  } catch (e) {
+    console.error(process.pid, "--- Failed to initialize InnerTube. Trying again in 10 seconds....");
+    console.error(e);
+
+    setTimeout(initInnerTube, 10000);
+  };
+};
+
+// Handle any unhandled promise rejection.
+process.on("unhandledRejection", console.error);
+
+initInnerTube();
