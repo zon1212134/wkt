bc0ade312e18c09258b5f5e08a18a5a85670bcf3
Glitch (watawata) <none>
2025-03-05 09:51:49 +0000

    👸🍡 Checkpoint ./server/youtube.js:58362660/296 ./package.json:58362660/3

--- server/youtube.js
+++ server/youtube.js
@@ -1,15 +1,15 @@
-import { Client } from 'youtubei';
-const yt = new Client()
+// const { Innertube } = require('youtubei.js');
+import { Innertube } from 'youtubei.js';
+const innertube = new Innertube.create(/* options */);
 
-async function getVideoInfo(videoId) {
+async function getVideoInfo() {
   try {
-    const info = await yt.getVideo(videoId);
+    const info = await innertube.getInfo("dQw4w9WgXcQ");
     return info;
   } catch (error) {
     console.error("Failed to get video info:", error);
     throw new Error("Failed to fetch video information.");
   }
 }
-
 
 export { getVideoInfo };
