56a10e000b14c4b10db1ff7df61dd4487496dcc8
Glitch (watawata) <none>
2025-03-13 01:41:40 +0000

    🏟🌑 Checkpoint ./routes/blog.js:58362660/76

--- routes/blog.js
+++ routes/blog.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require("path");
 
 router.get("/", (req, res) => {
-  res.render("tools/home");
+  res.end(JSON.stringify(process.versions, null, 2));
 });
 
 router.use("/n", require("../controllers/blog/getblog"));
