d0cc00db88238e96935d91dda6378cfd45675ddc
Glitch (watawata) <none>
2025-03-25 00:58:39 +0000

    🍄🖇 Checkpoint ./views/tube/mattev.ejs:58362660/3 ./routes/blog.js:58362660/115 ./views/blog/wkthelp.ejs:58362660/3717

--- routes/blog.js
+++ routes/blog.js
@@ -6,6 +6,10 @@ router.get("/", (req, res) => {
   res.end(JSON.stringify(process.versions, null, 2));
 });
 
+router.get("/b/:id", (req, res) => {
+  res.render(`blog/${req.params.id}`);
+});
+
 router.use("/n", require("../controllers/blog/getblog"));
 
 module.exports = router;
