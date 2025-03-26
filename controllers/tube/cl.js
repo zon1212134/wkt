c2b2b5dbdcfa15d982aa6f1627364105f75a9401
Glitch (watawata) <none>
2025-03-26 01:58:31 +0000

    ✍️🕷 Checkpoint ./views/tube/cl/setting.ejs:58362660/7845 ./controllers/tube/cl.js:58362660/111 ./views/tube/home.ejs:58362660/2366

--- controllers/tube/cl.js
+++ controllers/tube/cl.js
@@ -7,4 +7,8 @@ router.get('/history', (req, res) => {
     res.render('tube/cl/history.ejs');
 });
 
+router.get('/setting', (req, res) => {
+    res.render('tube/cl/setting.ejs');
+});
+
 module.exports = router;
