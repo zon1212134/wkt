c1a1bfd9b3e976f0f0e7539f1a4ba1e31fbf592b
Glitch (watawata) <none>
2025-03-28 01:48:40 +0000

    🔑💳 Checkpoint ./public/js/tube.js:58362660/7167 ./views/tube/cl/history.ejs:58362660/3 ./views/tube/cl/serhistory.ejs:58362660/312

--- public/js/tube.js
+++ public/js/tube.js
@@ -1,18 +1,34 @@
 $(function () {
     $('#searchbox').autocomplete({
         source: function (request, response) {
-            $.ajax({
-                url: '/wkt/back/suggest',
-                data: { keyword: request.term },
-                success: function (data) {
-                    response(data);
-                }
-            });
+            let searchTerm = request.term.trim();
+            if (searchTerm.length > 0) {
+                $.ajax({
+                    url: '/wkt/back/suggest',
+                    data: { keyword: searchTerm },
+                    success: function (data) {
+                        response(data.length > 0 ? data : getSearchHistory());
+                    },
+                    error: function () {
+                        response(getSearchHistory());
+                    }
+                });
+            } else {
+                response(getSearchHistory());
+            }
         },
         delay: 1,
+        minLength: 0,
         select: function (event, ui) {
             $('#searchbox').val(ui.item.value);
             $('#searchForm').submit();
         }
+    });
+    function getSearchHistory() {
+        let history = JSON.parse(localStorage.getItem('searchHistory')) || [];
+        return history;
+    }
+    $('#searchbox').on('focus', function () {
+        $(this).autocomplete('search', '');
     });
 });
