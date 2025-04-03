e81afacb7bb528c3beb298c2743b2d8532446b51
サダラマグラ <135095160+wakame02@users.noreply.github.com>
2025-04-03 09:02:04 +0000

    Update youtube.js

--- controllers/tube/youtube.js
+++ controllers/tube/youtube.js
@@ -9,8 +9,13 @@ router.get('/edu/:id', async (req, res) => {
   const videoId = req.params.id;
   try {
     const videoInfo = await serverYt.infoGet(videoId);
-    const response = await axios.get("https://wktedutube.glitch.me");
-    
+    let response;
+    try{
+      response = await axios.get("https://wktedutube.glitch.me");
+    } catch (nestedError) {
+      console.log(nestedError.message);
+      response = await axios.get("https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLg3df0uJm54ZJvlS9Pl8HyxJuvvZ3_zwc5cK8xZ-AyLNSHI79AQ_yqQWf4OFMEiUnQ21eeUC8tkTeW9h0l5065qOGt1DOfbVEYgPh2Mjkp4IJPzIJEPH-esCzd_40jdSG0rPPPkHgfyusNwOhtb5txF___c4a8eZLxd2zV2IGtYvQEcoJDV7R1vslzoGsJ45YxPAiLlSdDhVTzLJz-W7MUlll0kO5dY7l2KsxQrjpIPV5wIXmLz0lUrYPAy1F5BLV2C-y4SdtahNE6dwPwpvj7ElLq6K1RQgquUGaxF&lib=M91KG9TratNYR8mY3BgsDxXLpiZUpG_6w");
+    }
     const ytinfo = response.data;
     const videosrc = `https://www.youtubeeducation.com/embed/${videoId}${ytinfo}`;
           
