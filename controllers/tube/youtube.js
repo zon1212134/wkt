470dfa51efa3ec3f96d911b82ef6a22cae1a09c5
サダラマグラ <135095160+wakame02@users.noreply.github.com>
2025-04-03 12:02:50 +0000

    Update youtube.js

--- controllers/tube/youtube.js
+++ controllers/tube/youtube.js
@@ -5,23 +5,28 @@ const path = require("path");
 const http = require('http');
 const serverYt = require("../../server/youtube.js");
 
+async function getYtInfo() {
+  const urls = [
+    "https://wktedutube.glitch.me",
+    "https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLg3df0uJm54ZJvlS9Pl8HyxJuvvZ3_zwc5cK8xZ-AyLNSHI79AQ_yqQWf4OFMEiUnQ21eeUC8tkTeW9h0l5065qOGt1DOfbVEYgPh2Mjkp4IJPzIJEPH-esCzd_40jdSG0rPPPkHgfyusNwOhtb5txF___c4a8eZLxd2zV2IGtYvQEcoJDV7R1vslzoGsJ45YxPAiLlSdDhVTzLJz-W7MUlll0kO5dY7l2KsxQrjpIPV5wIXmLz0lUrYPAy1F5BLV2C-y4SdtahNE6dwPwpvj7ElLq6K1RQgquUGaxF&lib=M91KG9TratNYR8mY3BgsDxXLpiZUpG_6w",
+    "https://raw.githubusercontent.com/wakame02/wktopu/refs/heads/main/edu.text"
+  ];
+  for (const url of urls) {
+    try {
+      const response = await axios.get(url);
+      return response.data;
+    } catch (error) {
+      console.log(`ytinfo ${url}: ${error.message}`);
+    }
+  }
+  throw new Error('必要なデータを取得できませんでした。');
+}
+
 router.get('/edu/:id', async (req, res) => {
   const videoId = req.params.id;
   try {
     const videoInfo = await serverYt.infoGet(videoId);
-    let response;
-    try{
-      response = await axios.get("https://wktedutube.glitch.me");
-    } catch (nestedError) {
-      console.log(nestedError.message);
-      try {
-        response = await axios.get("https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLg3df0uJm54ZJvlS9Pl8HyxJuvvZ3_zwc5cK8xZ-AyLNSHI79AQ_yqQWf4OFMEiUnQ21eeUC8tkTeW9h0l5065qOGt1DOfbVEYgPh2Mjkp4IJPzIJEPH-esCzd_40jdSG0rPPPkHgfyusNwOhtb5txF___c4a8eZLxd2zV2IGtYvQEcoJDV7R1vslzoGsJ45YxPAiLlSdDhVTzLJz-W7MUlll0kO5dY7l2KsxQrjpIPV5wIXmLz0lUrYPAy1F5BLV2C-y4SdtahNE6dwPwpvj7ElLq6K1RQgquUGaxF&lib=M91KG9TratNYR8mY3BgsDxXLpiZUpG_6w");
-      } catch (secondError) {
-        consolelog(secondError.message);
-        response = await axios.get("https://raw.githubusercontent.com/wakame02/wktopu/refs/heads/main/edu.text");
-      }
-    }
-    const ytinfo = response.data;
+    const ytinfo = await getYtInfo();
     const videosrc = `https://www.youtubeeducation.com/embed/${videoId}${ytinfo}`;
           
     res.render('tube/umekomi/edu.ejs', {videosrc, videoInfo, videoId});
@@ -36,8 +41,8 @@ router.get('/edu/:id', async (req, res) => {
 
 router.get('/edurl', async (req, res) => {
   try {
-    const ytinfo = await axios.get("https://wktedutube.glitch.me");  
-    res.send(`${ytinfo.data}`);
+    const ytinfo = await getYtInfo();
+    res.send(`${ytinfo}`);
   } catch (error) {
      res.status(500).send(error);
   }
