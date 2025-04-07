bc94b031da053dd756995e071546b92d8384ca0f
サダラマグラ <135095160+wakame02@users.noreply.github.com>
2025-04-07 17:58:47 +0900

    Update getvideo.js

--- controllers/tube/getvideo.js
+++ controllers/tube/getvideo.js
@@ -62,7 +62,19 @@ router.get('/:id', async (req, res) => {
          videoData = await wakamess.getYouTube(videoId);
         }
       }
-      const videoInfo = await serverYt.infoGet(videoId);
+      const Info = await serverYt.infoGet(videoId);
+      const videoInfo = {
+        title: Info.primary_info.title.text || "",
+        channelId: Info.secondary_info.owner.author.id || "",
+        channelIcon: Info.secondary_info.owner.author.thumbnails[0].url || '',
+        channelName: Info.secondary_info.owner.author.name || "",
+        channelSubsc: Info.secondary_info.owner.subscriber_count.text || "",
+        published: Info.primary_info.published,
+        viewCount: Info.primary_info.view_count.short_view_count?.text || Info.primary_info.view_count.view_count?.text || "",
+        likeCount: Info.primary_info.menu.top_level_buttons.short_like_count || Info.primary_info.menu.top_level_buttons.like_count || Info.basic_info.like_count || "",
+        description: Info.secondary_info.description.text || "",
+        watch_next_feed: Info.watch_next_feed || "",
+      };
       res.render('tube/watch.ejs', { videoData, videoInfo, videoId, baseUrl });
   } catch (error) {
       const shufServerUrls = shuffleArray([...serverUrls]);
