89b5babc96b4ca2fdb12b1d0293aefef3419f47e
サダラマグラ <135095160+wakame02@users.noreply.github.com>
2025-05-27 01:14:06 +0000

    Update wakame.js

--- server/wakame.js
+++ server/wakame.js
@@ -7,7 +7,7 @@ const MAX_TIME = 10000;
 
 async function getapis() {
     try {
-        const response = await axios.get('https://wtserver.glitch.me/apis');
+        const response = await axios.get('https://raw.githubusercontent.com/wakame02/wktopu/refs/heads/main/inv.json');
         apis = await response.data;
         console.log('データを取得しました:', apis);
     } catch (error) {
