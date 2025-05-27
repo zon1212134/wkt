d28c74f4d1614e34777aec3d32bbedc4c13c6e19
サダラマグラ <135095160+wakame02@users.noreply.github.com>
2025-05-27 06:14:59 +0000

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cors());
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-    if (req.cookies.loginok !== 'ok' && !req.path.includes('login') && !req.path.includes('api')) 
+    if (req.cookies.loginok !== 'ok' && !req.path.includes('login') && !req.path.includes('back')) 
         return res.redirect('/login');
     } else {
         next();
