9c8b93d41c17f5c44b3c60415d109476dd34bf5b
サダラマグラ <135095160+wakame02@users.noreply.github.com>
2025-05-27 06:25:28 +0000

    Update server.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ let client;
 app.use(compression());
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
+app.use(cookieParser());
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
-app.use(cookieParser());
 
 app.use((req, res, next) => {
     if (req.cookies.loginok !== 'ok' && !req.path.includes('login') && !req.path.includes('back')) 
